c6d80827533d393a5e7eaf6039da2356e8d15bab
jasonyappp <Jasonyap102204@gmail.com>
2025-11-18 21:52:02 +0800

    version 2 design login By khaijie

--- login.php
+++ login.php
@@ -1,27 +1,34 @@
 <?php
-// login.php - MoeMoePetMart Login
-$headerTitle = "Welcome Back!";
-$pageTitle = "Login • MoeMoePetMart";
+// login.php - FINAL VERSION (Header + Footer + Perfect Design)
+$headerTitle = "MoeMoePetMart";
+$pageTitle = "Login - MoeMoePetMart";
 ob_start();
 ?>
 
-<div class="login-container">
-  <div class="paw-icon">🐾</div>
-  <h1>MoeMoePetMart</h1>
-  <p class="tagline">Your favorite pet shop is waiting for you!</p>
+<div class="login-full-container">
+    <div class="login-box">
+        <div class="petshop-icon">
+            <img src="images/pet-shop.png" alt="MoeMoePetMart" width="90" height="90">
+        </div>
 
-  <form action="dashboard.php" method="POST">
-    <input type="text" name="username" placeholder="Username or Email" required />
-    <input type="password" name="password" placeholder="Password" required />
-    
-    <button type="submit" class="btn-login">
-      Login Now
-    </button>
-  </form>
+        <h1>MoeMoePetMart</h1>
+        <p class="tagline">Your favorite pet shop is waiting for you!</p>
 
-  <div class="extra-links">
-    <a href="#">Forgot Password?</a> • <a href="register.php">Create Account</a>
-  </div>
+        <form action="dashboard.php" method="POST">
+            <div class="input-group">
+                <input type="text" name="username" placeholder="Username or Email" required autocomplete="username">
+            </div>
+            <div class="input-group">
+                <input type="password" name="password" placeholder="Password" required autocomplete="current-password">
+            </div>
+            <button type="submit" class="btn-login">Login Now</button>
+        </form>
+
+        <div class="extra-links">
+            <a href="#">Forgot Password?</a> • <a href="register.php">Create Account</a>
+        </div>
+    </div>
+</div>
 
 <?php
 $content = ob_get_clean();
